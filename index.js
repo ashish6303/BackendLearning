caf4ee63ceae6bc9a1a51a55763025177ea1b30c
ashish6303 <ashish6303@gmail.com>
2024-08-09 02:17:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/name', (req, res)=>{
+    res.send("My name is ashish prakash singh");
+})
+
+app.listen(process.config.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
